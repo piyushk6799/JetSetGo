b1c19e63b54c87550c42207e2717d111be60b402
piyushk6799 <piyushkulkarnilm@gmail.com>
2023-09-15 19:19:31 +0530

    added php

---
+++ jetsetgo/contact.php
@@ -0,0 +1,28 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get form data
+    $name = $_POST["fullname"];
+    $activities = $_POST["activities"];
+    $destination = $_POST["destination"];
+    $date = $_POST["date-start"];
+
+    // Set recipient email address
+    $to = "piyushkulkarilm@gmail.com";
+
+    // Set email subject
+    $subject = "New Trip Booking Request";
+
+    // Compose the email message
+    $message = "Name: $name\n";
+    $message .= "Activities: $activities\n";
+    $message .= "Destination: $destination\n";
+    $message .= "Date Travel: $date\n";
+
+    // Send the email
+    mail($to, $subject, $message);
+
+    // Redirect to a thank you page
+    header("Location: index.html");
+    exit;
+}
+?>
